b598b38a7c3a67a7413d62f55d2282d9fbc7c27a
Alexandar Valov <alexvulov123@gmail.com>
2025-05-28 14:12:25 +0300

    Footer & Status Container

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,5 +5,41 @@
     <v-main>
       <slot />
     </v-main>
+
+    <v-footer
+      app
+      class="flex-column bg-white py-4 text-center text-medium-emphasis"
+      style="position: relative"
+    >
+      <!-- Copyright Section -->
+      <v-row class="pt-2 d-flex justify-end align-end">
+        <v-col>
+          <!-- SKYWARE Group with Tooltip -->
+          <client-only>
+            <v-tooltip location="bottom" :open-delay="700" :close-delay="300">
+              <template v-slot:activator="{ props }">
+                <NuxtLink
+                  to="http://www.skyware-group.com/"
+                  target="_blank"
+                  v-bind="props"
+                  class="text-body-2 text-medium-emphasis text-decoration-none"
+                >
+                  SKYWARE Group
+                </NuxtLink>
+              </template>
+
+              <!-- Tooltip text -->
+              <span>Посетете уеб сайтът на SKYWARE Group</span>
+            </v-tooltip>
+
+            <!-- Current year -->
+            <span class="text-body-2">© {{ new Date().getFullYear() }}.</span>
+          </client-only>
+
+          <!-- Current version -->
+          <span class="text-body-2"> Всички права запазени. </span>
+        </v-col>
+      </v-row>
+    </v-footer>
   </v-app>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -66,5 +66,124 @@ const rightTabs = [{ value: 3, icon: "mdi-code-json", label: "Swagger" }];
         </v-tabs>
       </div>
     </div>
+
+    <!-- Page Title -->
+    <div class="px-4 my-4">
+      <div class="mx-auto d-flex flex-wrap align-center" style="max-width: 1320px">
+        <h3>Информация за Rila</h3>
+        <v-btn
+          icon="mdi-refresh"
+          variant="text"
+          color=""
+          class="opacity-50"
+          style="width: 44px; height: 44px"
+        ></v-btn>
+        <v-checkbox :model-value="true" :hide-details="true" color="primary"></v-checkbox>
+        <h3>Автоматично презареждане</h3>
+      </div>
+    </div>
+
+    <!-- Status Container -->
+    <div class="px-4">
+      <div
+        class="mx-auto d-flex flex-column border border-grey-lighten-2 rounded"
+        style="max-width: 1320px"
+      >
+        <!-- Status -->
+        <span class="border-b border-grey-lighten-2 py-4 px-6">Статус</span>
+
+        <v-row class="py-4 px-6 w-100" align="start" gap="4">
+          <!-- Version -->
+          <v-col cols="12" sm="6" md="4" lg="3" class="d-flex flex-column ga-2">
+            <span
+              class="text-uppercase font-weight-bold opacity-60"
+              style="font-size: 0.625rem; letter-spacing: 0.04em"
+            >
+              Версия
+            </span>
+
+            <div class="d-flex align-center ga-0">
+              <span class="text-body-2"> 2.6.0 </span>
+              <v-btn variant="plain" icon="mdi-content-copy" color="" size="x-small"></v-btn>
+            </div>
+          </v-col>
+
+          <!-- Instance -->
+          <v-col cols="12" sm="6" md="4" lg="3" class="d-flex flex-column ga-2">
+            <span
+              class="text-uppercase font-weight-bold opacity-60"
+              style="font-size: 0.625rem; letter-spacing: 0.04em"
+            >
+              Инстанция
+            </span>
+
+            <div class="d-flex align-center ga-0">
+              <span class="text-body-2"> МДЛ Ботевград ЕООД-dev </span>
+              <v-btn variant="plain" icon="mdi-content-copy" color="" size="x-small"></v-btn>
+            </div>
+          </v-col>
+
+          <!-- Server Time -->
+          <v-col cols="12" sm="6" md="4" lg="3" class="d-flex flex-column ga-2">
+            <span
+              class="text-uppercase font-weight-bold opacity-60"
+              style="font-size: 0.625rem; letter-spacing: 0.04em"
+            >
+              Сървърно време
+            </span>
+
+            <div class="d-flex align-center ga-0">
+              <span class="text-body-2"> 27/05/2025 17:49:59 </span>
+              <v-btn variant="plain" icon="mdi-content-copy" color="" size="x-small"></v-btn>
+            </div>
+          </v-col>
+
+          <!-- User -->
+          <v-col cols="12" sm="6" md="4" lg="3" class="d-flex flex-column ga-2">
+            <span
+              class="text-uppercase font-weight-bold opacity-60"
+              style="font-size: 0.625rem; letter-spacing: 0.04em"
+            >
+              Потребител
+            </span>
+
+            <div class="d-flex align-center ga-0">
+              <span class="text-body-2">Rila</span>
+              <v-btn variant="plain" icon="mdi-content-copy" color="" size="x-small"></v-btn>
+            </div>
+          </v-col>
+
+          <!-- Internet Connectivity -->
+          <v-col cols="12" sm="6" md="4" lg="3" class="d-flex flex-column ga-2">
+            <span
+              class="text-uppercase font-weight-bold opacity-60"
+              style="font-size: 0.625rem; letter-spacing: 0.04em"
+            >
+              Достъп до интернет
+            </span>
+
+            <div class="d-flex align-center ga-0">
+              <span class="text-body-2">Свързан</span>
+              <v-btn variant="plain" icon="mdi-content-copy" color="" size="x-small"></v-btn>
+            </div>
+          </v-col>
+
+          <!-- Environment -->
+          <v-col cols="12" sm="6" md="4" lg="3" class="d-flex flex-column ga-2">
+            <span
+              class="text-uppercase font-weight-bold opacity-60"
+              style="font-size: 0.625rem; letter-spacing: 0.04em"
+            >
+              Среда
+            </span>
+
+            <div class="d-flex align-center ga-0">
+              <span class="text-body-2">Тестова</span>
+              <v-btn variant="plain" icon="mdi-content-copy" color="" size="x-small"></v-btn>
+            </div>
+          </v-col>
+        </v-row>
+      </div>
+    </div>
   </v-container>
 </template>
